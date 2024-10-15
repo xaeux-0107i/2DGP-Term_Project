3f8760903c09fadd1df7c64189b523048ca6f0f9
xaeux0107 <ekdms1324@gmail.com>
2024-10-15 13:16:17 +0900

    게임루프 틀 작성 및 종료 기능 추가
    게임루프 틀 작성
    
    esc 또는 창닫기 버튼을 누르면
    프로그램 종료

--- cookie_run/cookie_run.py
+++ cookie_run/cookie_run.py
@@ -4,19 +4,52 @@ class Background:
     def __init__(self):
         self.image = load_image('Background1.png')
         if self.image is None:
-            print("이미지 로드 실패")
-
+            print("배경 이미지 로드 실패")
     def draw(self):
         self.image.clip_draw(0, 0, 567, 320, 400, 300, 800, 600)
+    def update(self):
+        pass
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+def reset_world():
+    global running
+    global back_ground
+    global world
+
+    running = True
+    world = []
+
+    back_ground = Background()
+    world.append(back_ground)
+
+def update_world():
+    for o in world:
+        o.update()
+    pass
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
+
 
 open_canvas()
+reset_world()
 
-back_ground = Background()
-
-back_ground.draw()
-
-update_canvas()
-delay(10)
+# game loop
+while running:
+    handle_events()
+    update_world()
+    render_world()
 
 close_canvas()
 
