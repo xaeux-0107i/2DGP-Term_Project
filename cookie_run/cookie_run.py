aeab83cddef1c02627de320808ada30df91e5034
xaeux0107 <ekdms1324@gmail.com>
2024-10-16 23:02:51 +0900

    불꽃 이미지 및 클래스 추가
    불꽃 그리기 작동 확인

--- cookie_run/cookie_run.py
+++ cookie_run/cookie_run.py
@@ -10,6 +10,16 @@ class Background:
     def update(self):
         pass
 
+class Flame:
+    def __init__(self):
+        self.image = load_image('flame.png')
+        if self.image is None:
+            print("배경 이미지 로드 실패")
+    def draw(self):
+        self.image.clip_draw(0, 0, 860, 316, 400, 200, 800, 400)
+    def update(self):
+        pass
+
 def handle_events():
     global running
     events = get_events()
@@ -23,12 +33,15 @@ def reset_world():
     global running
     global back_ground
     global world
+    global flame
 
     running = True
     world = []
 
     back_ground = Background()
     world.append(back_ground)
+    flame = Flame()
+    world.append(flame)
 
 def update_world():
     for o in world:
