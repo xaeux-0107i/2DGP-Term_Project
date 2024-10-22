dd7eecbd4322cb5ea873a83d63e0590d0d0b0a42
xaeux0107 <ekdms1324@gmail.com>
2024-10-22 16:37:52 +0900

    1단 점프 동작 및 애니메이션 추가

--- cookie_run/cookie_run.py
+++ cookie_run/cookie_run.py
@@ -36,10 +36,12 @@ class Cookie:
         self.jump_count = 0
         self.x = 170
         self.y = 180
+        self.dy = 0
         self.image_running = load_image('brave_cookie_running.png') # 칸 당 가로: 270  세로: 268
         self.image_sliding = load_image('brave_cookie_sliding.png')  # 칸 당 가로: 269  세로: 268
         self.image_jump = load_image('brave_cookie_jump.png') # 가로 270 세로 268
-        self.state = 0 # 0 - 달리기, 1 - 점프, 2- 슬라이딩, 3 - 캐릭터 사망
+        self.image_jump2 = load_image('brave_cookie_jump2.png') #
+        self.state = 0 # 0 - 달리기, 1 - 점프, 2- 슬라이딩, 3 - 2단 점프 4 - 캐릭터 사망
     def draw(self):
         if self.state == 0: # 달리기
             self.image_running.clip_draw(self.frame + 2 + 270*self.frame, 0, 260, 268, self.x, self.y, 200, 200)
@@ -47,20 +49,35 @@ class Cookie:
             self.image_jump.clip_draw(self.frame + 270*self.frame, 0, 265, 268, self.x, self.y, 200, 200)
         if self.state == 2: # 슬라이딩
             self.image_sliding.clip_draw(self.frame + 270*self.frame, 0, 265, 268, self.x, self.y, 200, 200)
+        if self.state == 3: # 2단 점프
+            self.image_jump2.clip_draw(self.frame + 1 + 270 * self.frame, 0, 265, 268, self.x, self.y, 200, 200)
     def update(self):
         if self.state == 0: # 달리기
             self.frame = (self.frame + 1) % 4
-        if self.state == 1: # 점프
-            if cookie.frame == 0:
-                pass
-            if cookie.frame == 1:
-                pass
-            if cookie.frame == 2:
-                pass
-            if cookie.frame == 4:
-                pass
+        if self.state == 1: # 1단 점프
+            if self.jump_count < 5:
+                self.frame = 0
+                self.y += self.dy
+                self.dy -= 1
+            elif self.jump_count < 10:
+                self.y += self.dy
+                self.dy -= 1
+                self.frame = 1
+            elif self.jump_count < 20:
+                self.y -= self.dy
+                self.dy += 1
+                self.frame = 1
+            elif self.jump_count < 22:
+                self.y -= self.dy
+                self.frame = 3
+            else:
+                self.state = 0  # 점프가 끝나면 달리기 상태로 돌아감
+                self.jump_count = 0  # 점프 카운트 초기화
+                self.y = 180
+            self.jump_count += 1
         if self.state == 2: # 슬라이딩
             self.frame = (self.frame + 1) % 2
+            self.y = 180
         pass
 
 def handle_events():
@@ -73,10 +90,11 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
-            cookie.state = 1  # 점프
-            if cookie.jump_count == 0: # 1단 점프
+            if cookie.state == 0 or cookie.state == 2 :
+                cookie.state = 1  # 점프
+                cookie.jump_count = 0
                 cookie.frame = 0
-                cookie.y += 10
+                cookie.dy = 15
         elif event.type == SDL_KEYDOWN and event.key == SDLK_DOWN:
             cookie.state = 2 # 슬라이딩
         elif event.type == SDL_KEYUP and event.key == SDLK_DOWN:
