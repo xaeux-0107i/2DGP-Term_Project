99d667cf54d36a2d444fe7a02d0008f71e24c86b
xaeux0107 <ekdms1324@gmail.com>
2024-10-31 11:58:46 +0900

    배경 및 불꽃 클래스 분리
    class Background, class Flame
    ->background.py

---
+++ cookie_run/backgound.py
@@ -0,0 +1,37 @@
+from pico2d import load_image
+
+from cookie_run.cookie_run import speed
+
+
+class Background:
+    def __init__(self):
+        self.image = load_image('Background1.png')
+        self.speed = speed
+        self.dx = 0
+        if self.image is None:
+            print("배경 이미지 로드 실패")
+    def draw(self):
+        self.image.clip_draw(0, 0, 567, 320, 400 - self.dx, 300, 800, 600)
+        self.image.clip_draw(0, 0, 567, 320, 1200 - self.dx, 300, 800, 600)
+    def update(self):
+        if self.dx > 800:
+            self.dx = 0
+        else:
+            self.dx += self.speed
+
+
+class Flame:
+    def __init__(self):
+        self.image = load_image('flame.png')
+        self.dx = 0
+        self.speed = speed
+        if self.image is None:
+            print("불꽃 이미지 로드 실패")
+    def draw(self):
+        self.image.clip_draw(0, 0, 860, 316, 400 - int(self.dx), 200, 800, 400)
+        self.image.clip_draw(0, 0, 860, 316, 1200 - int(self.dx), 200, 800, 400)
+    def update(self):
+        if self.dx > 800:
+            self.dx = 0
+        else:
+            self.dx += self.speed
