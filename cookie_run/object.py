ebabc72638480f5348c2b066bbc46f31e00f2c5b
xaeux0107 <ekdms1324@gmail.com>
2024-10-31 16:21:50 +0900

    소세지 포크 장애물1 생성 구현

--- cookie_run/object.py
+++ cookie_run/object.py
@@ -50,3 +50,19 @@ class Olive:
         if self.x < -50:  # -50은 울타리 너비에 맞게 조정
             self.x += 900  # jelly2의 오른쪽 끝으로 이동
         pass
+
+class ForkS1:
+    def __init__(self, x):
+        self.image = load_image('object_image/obstacles/fork_with_sausage.png')
+        self.x = x #50
+        self.dx = 2*speed
+        if self.image is None:
+            print("장애물 이미지 로드 실패")
+    def draw(self):
+        self.image.clip_draw(0, 0, 106, 193, self.x, 160, 80, 160)
+    def update(self):
+        self.x -= self.dx
+        # 만약 젤리가 화면 왼쪽 밖으로 이동하면 위치를 오른쪽 끝으로 재설정
+        if self.x < -50:  # -50은 울타리 너비에 맞게 조정
+            self.x += 900  # jelly2의 오른쪽 끝으로 이동
+        pass
