1ea3c565e7c4ff366b2483e873fe472228765ec5
xaeux0107 <ekdms1324@gmail.com>
2024-10-31 12:01:19 +0900

    바닥 펜스 및 젤리 클래스 분리
    class Fence, class Jelly
    ->object.py

--- cookie_run/cookie_run.py
+++ cookie_run/cookie_run.py
@@ -1,41 +1,10 @@
 from pico2d import *
 
 from cookie_run.backgound import Background, Flame
+from cookie_run.object import Fence, Jelly
 
 global speed
 speed = 4
-
-class Fence:
-    def __init__(self, x):
-        self.image = load_image('oven_fence.png')
-        self.x = x #50
-        self.dx = 2*speed
-        if self.image is None:
-            print("펜스 이미지 로드 실패")
-    def draw(self):
-        self.image.clip_draw(0, 0, 124, 120, self.x, 40, 100, 80)
-    def update(self):
-        self.x -= self.dx
-        # 만약 울타리가 화면 왼쪽 밖으로 이동하면 위치를 오른쪽 끝으로 재설정
-        if self.x < -50:  # -50은 울타리 너비에 맞게 조정
-            self.x += 900  # fences2의 오른쪽 끝으로 이동
-        pass
-
-class Jelly:
-    def __init__(self, x):
-        self.image = load_image('jelly.png')
-        self.x = x #50
-        self.dx = 2*speed
-        if self.image is None:
-            print("젤리 이미지 로드 실패")
-    def draw(self):
-        self.image.clip_draw(0, 0, 55, 52, self.x, 130, 50, 50)
-    def update(self):
-        self.x -= self.dx
-        # 만약 젤리가 화면 왼쪽 밖으로 이동하면 위치를 오른쪽 끝으로 재설정
-        if self.x < -50:  # -50은 울타리 너비에 맞게 조정
-            self.x += 900  # jelly2의 오른쪽 끝으로 이동
-        pass
 
 class Cookie:
     def __init__(self):
@@ -132,10 +101,10 @@ class Cookie:
 def create_map():
     global fences1, fences2
     global jelly1, jelly2
-    fences1 = [Fence(i*100 + 50) for i in range (0, 9)]
+    fences1 = [Fence(i * 100 + 50) for i in range (0, 9)]
     fences2 = [Fence(i * 100 + 950) for i in range(0, 9)]
-    jelly1 = [Jelly(100*i + 50) for i in range(0,18)]
-    jelly2 = [Jelly(100*i + 950) for i in range(0, 18)]
+    jelly1 = [Jelly(100 * i + 50) for i in range(0, 18)]
+    jelly2 = [Jelly(100 * i + 950) for i in range(0, 18)]
     pass
 
 def handle_events():
---
+++ cookie_run/object.py
@@ -0,0 +1,37 @@
+from pico2d import load_image
+
+from cookie_run.cookie_run import speed
+
+
+class Fence:
+    def __init__(self, x):
+        self.image = load_image('oven_fence.png')
+        self.x = x #50
+        self.dx = 2*speed
+        if self.image is None:
+            print("펜스 이미지 로드 실패")
+    def draw(self):
+        self.image.clip_draw(0, 0, 124, 120, self.x, 40, 100, 80)
+    def update(self):
+        self.x -= self.dx
+        # 만약 울타리가 화면 왼쪽 밖으로 이동하면 위치를 오른쪽 끝으로 재설정
+        if self.x < -50:  # -50은 울타리 너비에 맞게 조정
+            self.x += 900  # fences2의 오른쪽 끝으로 이동
+        pass
+
+
+class Jelly:
+    def __init__(self, x):
+        self.image = load_image('jelly.png')
+        self.x = x #50
+        self.dx = 2*speed
+        if self.image is None:
+            print("젤리 이미지 로드 실패")
+    def draw(self):
+        self.image.clip_draw(0, 0, 55, 52, self.x, 130, 50, 50)
+    def update(self):
+        self.x -= self.dx
+        # 만약 젤리가 화면 왼쪽 밖으로 이동하면 위치를 오른쪽 끝으로 재설정
+        if self.x < -50:  # -50은 울타리 너비에 맞게 조정
+            self.x += 900  # jelly2의 오른쪽 끝으로 이동
+        pass
