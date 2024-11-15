7c95307cbf33d14e220a60853be0fa6b935e3910
xaeux0107 <ekdms1324@gmail.com>
2024-11-15 23:38:56 +0900

    Giant class 추가
    거대화 물약 아이템 클래스 추가

--- cookie_run/item.py
+++ cookie_run/item.py
@@ -18,10 +18,23 @@ class Energy:
         self.count += 1
         if self.count % 8 ==0:
             self.frame = (self.frame+1)%4
-        pass
-    pass
 
 class Giant:
+    def __init__(self, x):
+        self.image = load_image('object_image/jelly_and_items/giant_item.png')
+        self.x = x #50
+        self.dx = 2*speed
+        self.frame = 0
+        self.count = 0
+        if self.image is None:
+            print("거대화 이미지 로드 실패")
+    def draw(self):
+        self.image.clip_draw(91 * self.frame, 0, 90, 88, self.x, 300, 100, 100)
+    def update(self):
+        #self.x -= self.dx
+        self.count += 1
+        if self.count % 8 ==0:
+            self.frame = (self.frame+1)%4
     pass
 
 class Sprint:
