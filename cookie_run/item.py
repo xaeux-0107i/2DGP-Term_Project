422765e3912b5291a15df4912bf065f6837769ef
xaeux0107 <ekdms1324@gmail.com>
2024-11-16 23:21:36 +0900

    각 아이템 클래스에 충돌 처리 및 바운딩 볼륨 함수 추가

--- cookie_run/item.py
+++ cookie_run/item.py
@@ -1,56 +1,76 @@
 from pico2d import load_image
+import game_world
 global speed
 speed = 4
 
 class Energy:
-    def __init__(self, x):
+    def __init__(self, x, y):
         self.image = load_image('object_image/jelly_and_items/energy_item.png')
         self.x = x #50
+        self.y = y
         self.dx = 2*speed
         self.frame = 0
         self.count = 0
         if self.image is None:
             print("포션 이미지 로드 실패")
     def draw(self):
-        self.image.clip_draw(146 * self.frame, 0, 140, 142, self.x, 300, 100, 100)
+        self.image.clip_draw(146 * self.frame, 0, 140, 142, self.x, self.y, 100, 100)
     def update(self):
         #self.x -= self.dx
         self.count += 1
         if self.count % 8 ==0:
             self.frame = (self.frame+1)%4
+    def get_bb(self):
+        return self.x - 25, self.y - 25, self.x + 25, self.y + 25
+    def handle_collision(self, group, other):
+        # fill here
+        if group == 'cookie:energy':
+            game_world.remove_object(self)
 
 class Giant:
-    def __init__(self, x):
+    def __init__(self, x, y):
         self.image = load_image('object_image/jelly_and_items/giant_item.png')
         self.x = x #50
+        self.y = y
         self.dx = 2*speed
         self.frame = 0
         self.count = 0
         if self.image is None:
             print("거대화 이미지 로드 실패")
     def draw(self):
-        self.image.clip_draw(91 * self.frame, 0, 90, 88, self.x, 300, 100, 100)
+        self.image.clip_draw(91 * self.frame, 0, 90, 88, self.x, self.y, 100, 100)
     def update(self):
         #self.x -= self.dx
         self.count += 1
         if self.count % 8 ==0:
             self.frame = (self.frame+1)%4
-    pass
+    def get_bb(self):
+        return self.x - 25, self.y - 25, self.x + 25, self.y + 25
+    def handle_collision(self, group, other):
+        # fill here
+        if group == 'cookie:giant':
+            game_world.remove_object(self)
 
 class Sprint:
-    def __init__(self, x):
+    def __init__(self, x, y):
         self.image = load_image('object_image/jelly_and_items/dash_item.png')
         self.x = x #50
+        self.y = y
         self.dx = 2*speed
         self.frame = 0
         self.count = 0
         if self.image is None:
             print("질주화 이미지 로드 실패")
     def draw(self):
-        self.image.clip_draw(146 * self.frame, 0, 140, 142, self.x, 300, 100, 100)
+        self.image.clip_draw(146 * self.frame, 0, 140, 142, self.x, self.y, 100, 100)
     def update(self):
         #self.x -= self.dx
         self.count += 1
         if self.count % 8 ==0:
             self.frame = (self.frame+1)%4
-    pass
+    def get_bb(self):
+        return self.x - 25, self.y - 25, self.x + 25, self.y + 25
+    def handle_collision(self, group, other):
+        # fill here
+        if group == 'cookie:sprint':
+            game_world.remove_object(self)
