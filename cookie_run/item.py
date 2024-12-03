19660261ecd4fb4a29363b6a2cb9e9b5781f3c18
xaeux0107 <ekdms1324@gmail.com>
2024-12-03 14:14:36 +0900

    아이템 효과음 추가

--- cookie_run/item.py
+++ cookie_run/item.py
@@ -1,10 +1,12 @@
-from pico2d import load_image, draw_rectangle
+from pico2d import load_image, draw_rectangle, load_wav
 import game_world
 import play_mode
 global speed
 speed = 4
 
 class Energy:
+    energy_sound = None
+
     def __init__(self, x, y):
         self.image = load_image('object_image/jelly_and_items/energy_item.png')
         self.x = x #50
@@ -14,6 +16,8 @@ class Energy:
         self.count = 0
         if self.image is None:
             print("포션 이미지 로드 실패")
+        if not Energy.energy_sound:
+            Energy.energy_sound = load_wav('sounds/get_energy.wav')
     def draw(self):
         self.image.clip_draw(146 * self.frame, 0, 140, 142, self.x, self.y, 100, 100)
         if play_mode.collision_box:
@@ -29,8 +33,11 @@ class Energy:
         # fill here
         if group == 'cookie:energy':
             game_world.remove_object(self)
+            Energy.energy_sound.play(1)
 
 class Giant:
+    giant_sound = None
+
     def __init__(self, x, y):
         self.image = load_image('object_image/jelly_and_items/giant_item.png')
         self.x = x #50
@@ -40,6 +47,8 @@ class Giant:
         self.count = 0
         if self.image is None:
             print("거대화 이미지 로드 실패")
+        if not Giant.giant_sound:
+            Giant.giant_sound = load_wav('sounds/get_jelly.wav')
     def draw(self):
         self.image.clip_draw(92 * self.frame, 0, 88, 88, self.x, self.y, 100, 100)
         if play_mode.collision_box:
@@ -55,8 +64,11 @@ class Giant:
         # fill here
         if group == 'cookie:giant':
             game_world.remove_object(self)
+            Giant.giant_sound.play(1)
 
 class Sprint:
+    sprint_sound = None
+
     def __init__(self, x, y):
         self.image = load_image('object_image/jelly_and_items/dash_item.png')
         self.x = x #50
@@ -66,6 +78,8 @@ class Sprint:
         self.count = 0
         if self.image is None:
             print("질주화 이미지 로드 실패")
+        if not Sprint.sprint_sound:
+            Sprint.sprint_sound = load_wav('sounds/get_jelly.wav')
     def draw(self):
         self.image.clip_draw(92 * self.frame, 0, 88, 88, self.x, self.y, 100, 100)
         if play_mode.collision_box:
@@ -81,3 +95,4 @@ class Sprint:
         # fill here
         if group == 'cookie:sprint':
             game_world.remove_object(self)
+            Sprint.sprint_sound.play(1)
